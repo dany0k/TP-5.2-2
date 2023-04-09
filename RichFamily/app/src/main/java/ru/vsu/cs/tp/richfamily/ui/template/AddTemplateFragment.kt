b97dfef99885950397e832d50f87a946ae5ff278
DenPC <zmaev.vsu@gmail.com>
2023-04-09 21:29:35 +0300

    client1: add AddTemplateFragment

---
+++ RichFamily/app/src/main/java/ru/vsu/cs/tp/richfamily/ui/template/AddTemplateFragment.kt
@@ -0,0 +1,22 @@
+package ru.vsu.cs.tp.richfamily.ui.template
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.vsu.cs.tp.richfamily.R
+import ru.vsu.cs.tp.richfamily.databinding.FragmentAddTemplateBinding
+
+class AddTemplateFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddTemplateBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddTemplateBinding.inflate(inflater, container, false)
+        return inflater.inflate(R.layout.fragment_add_template, container, false)
+    }
+}
