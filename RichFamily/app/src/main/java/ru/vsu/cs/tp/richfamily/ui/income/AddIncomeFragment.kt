fdce08bf5592a6a953f20e1a62b3159dba9d868d
DenPC <zmaev.vsu@gmail.com>
2023-04-09 21:30:01 +0300

    client1: add AddIncomeFragment

---
+++ RichFamily/app/src/main/java/ru/vsu/cs/tp/richfamily/ui/income/AddIncomeFragment.kt
@@ -0,0 +1,22 @@
+package ru.vsu.cs.tp.richfamily.ui.income
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.vsu.cs.tp.richfamily.R
+import ru.vsu.cs.tp.richfamily.databinding.FragmentAddIncomeBinding
+
+class AddIncomeFragment: Fragment() {
+
+    private lateinit var binding: FragmentAddIncomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddIncomeBinding.inflate(inflater, container, false)
+        return inflater.inflate(R.layout.fragment_add_income, container, false)
+    }
+}
