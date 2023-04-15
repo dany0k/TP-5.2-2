eefff20eb54231c7dbbda55e08d4e1336bae05ee
SmPavel <76187376+SmPavel@users.noreply.github.com>
2023-04-15 20:41:54 +0300

    client3: add operations repository and data models

---
+++ RichFamily/app/src/main/java/ru/vsu/cs/tp/richfamily/model/Operations.kt
@@ -0,0 +1,33 @@
+package ru.vsu.cs.tp.richfamily.model
+
+import android.os.Parcelable
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
+import ru.vsu.cs.tp.richfamily.model.Category
+import ru.vsu.cs.tp.richfamily.model.Wallet
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+
+@Parcelize
+@Entity(tableName = "operations")
+
+data class Operations(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    @ColumnInfo(name = "wallet")
+    val wallet: Wallet,
+    @ColumnInfo(name = "date")
+    val date: LocalDateTime,
+    @ColumnInfo(name = "recipient")
+    val recipient: String,
+    @ColumnInfo(name = "category")
+    val category: Category,
+    @ColumnInfo(name = "sum")
+    val sum: Float,
+    @ColumnInfo(name = "comment")
+    val comment: String,
+) : Parcelable
