442511a6d4f9103c6d7b3c3d04511a6b4a32129a
Dmitriy Mamonov <dimon.5642014@yandex.ru>
2023-04-21 09:09:19 +0300

    server9: add views for user info and custom user update

--- RichFamily/RichFamily/settings.py
+++ RichFamily/RichFamily/settings.py
@@ -85,18 +85,18 @@ WSGI_APPLICATION = 'RichFamily.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-        #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        #}
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
-    },
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': os.getenv('POSTGRES_DB'),
+    #    'USER': os.getenv('POSTGRES_USER'),
+    #    'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+    #    'HOST': os.getenv('POSTGRES_HOST'),
+    #    'PORT': os.getenv('POSTGRES_PORT'),
+    #},
 }
 
 
