a31245ca2abf6ff19dc7e26ad1d6fbcc6ff2e13c
Daniil Zmaev <zmaev.vsu@gmail.com>
2023-05-17 23:42:24 +0300

    client5: add csv format export

--- RichFamily/app/src/main/java/ru/vsu/cs/tp/richfamily/utils/Constants.kt
+++ RichFamily/app/src/main/java/ru/vsu/cs/tp/richfamily/utils/Constants.kt
@@ -1,7 +1,9 @@
 package ru.vsu.cs.tp.richfamily.utils
 
 object Constants {
+    const val PERMISSION_GRANTED = "Доступ получен"
     const val CONS_TEXT = "РАСХОД"
     const val INCOME_TEXT = "ДОХОД"
     const val COMP_FIELDS_TOAST = "Заполните все поля!"
+    const val FILE_SAVE_SUCCESS = "Файл успешно загружен!"
 }
